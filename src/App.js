dcb4b3d676d4a394ab2cb4dd3f8ae649be92c9d3
jo-turn <94jhgmsg@gmail.com>
2021-01-16 00:57:05 +0900

    create css module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import Person from './Person/Person'
+
 import './App.css'
 
 class App extends Component {
@@ -44,11 +46,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightGreen',
+        color: 'black',
+      },
     }
 
     let person = null
@@ -69,14 +76,33 @@ class App extends Component {
           })}
         </div>
       )
+      // style.backgroundColor = 'red'
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black',
+      // }
     }
+
+    let classes = []
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
+    }
+
     return (
       <div className="App">
         <h1>Hi I'm React App</h1>
-        <p>This is really working!!</p>
-        <button style={style} onClick={this.togglePersonHandler}>
+        <p className={classes.join(' ')}>This is really working!!</p>
+        <botton
+          className="button"
+          alt={this.state.showPersons}
+          onClick={this.togglePersonHandler}
+        >
+          {' '}
           Toggle Person
-        </button>
+        </botton>
         {person}
       </div>
     )
